07e6edc24af46895763409a30f6bab5c9c77ab6a
Jerry Tann <jerry81@gmail.com>
2019-07-22 17:17:17 +0800

    lesson: components

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from './comps/MyLayout';
+
+
+export default function About() {
+    return (
+      <Layout>
+        <p>This is the about page</p>
+      </Layout>
+    )
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Link from 'next/link';
+import Layout from './comps/MyLayout';
 
 const Index = () => (
-  <div>
+  
+       <Layout>
 <Link href="/about">
   <button>Go to About Page</button>
 </Link>
-
-
     <p>Hello Next.js</p>
-  </div>
+  </Layout>
 );
 
 export default Index;
+
+
+
